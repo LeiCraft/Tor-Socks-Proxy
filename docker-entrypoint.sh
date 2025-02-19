619c8d8a50be345afc47fda581154c303ea80c98
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-19 12:57:18 +0000

    added ipv6 support for socks5

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,12 @@ cat << TOR_CONF > "/etc/tor/torrc"
 HardwareAccel 1
 Log notice stdout
 DataDirectory /var/lib/tor
+
 DNSPort 0.0.0.0:8853
+DNSPort [::]:8853
+
 SocksPort 0.0.0.0:9150
+SocksPort [::]:9150
 
 TOR_CONF
 
