4c7486adc88c7fabdc4c84453abdd0a684d5dafd
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-20 17:01:26 +0000

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 GH_BASE_URL="https://github.com/Socks5Balancer/Socks5BalancerAsio/releases/download/v1.3.6-fix/Socks5BalancerAsio-e0a05953f1989bc3d7cbf776f2e88832fd313259"
 
-#DOWNLOAD_URL="$GH_BASE_URL-DynamicSSL-ProxyHandshakeAuth-Linux.zip"
-DOWNLOAD_URL="$GH_BASE_URL-DynamicSSL-Normal-Linux.zip"
+DOWNLOAD_URL="$GH_BASE_URL-DynamicSSL-ProxyHandshakeAuth-Linux.zip"
+#DOWNLOAD_URL="$GH_BASE_URL-DynamicSSL-Normal-Linux.zip"
 
 HOME_DIR="/home/container"
 TMP_DIR="$HOME_DIR/tmp"
@@ -17,7 +17,6 @@ curl -L -o $TMP_DIR/socks5balancer.zip $DOWNLOAD_URL && \
     rm -rf $TMP_DIR/socks5balancer.zip $TMP_DIR/socks5-relay
 
 cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
-
 {
   "listenHost": "::",
   "listenPort": 9150,
@@ -30,7 +29,6 @@ cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
     }
   ]
 }
-
 SOCKS5_CONF
 
 /home/container/socks5-relay -c /home/container/socks-relay-config.json &
