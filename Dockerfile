b6f6865da2f3ff0fbe265c1d4d30d6bc3c0bb9e7
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-20 17:26:50 +0000

    update

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,19 @@ RUN apt update && \
     apt upgrade -y && \
     apt install -y tor obfs4proxy curl unzip
 
+RUN curl -L -o /tmp/socks5balancer.zip \
+    https://github.com/Socks5Balancer/Socks5BalancerAsio/releases/download/v1.3.6-fix/Socks5BalancerAsio-e0a05953f1989bc3d7cbf776f2e88832fd313259-DynamicSSL-ProxyHandshakeAuth-Linux.zip && \
+    unzip /tmp/socks5balancer.zip -d /tmp/socks5-relay && \
+    mv /tmp/socks5-relay/Socks5BalancerAsio /usr/bin/socks5-relay && \
+    chmod 755 /usr/bin/socks5-relay && \
+    rm -rf /tmp/socks5balancer.zip /tmp/socks5-relay
+
 RUN adduser --disabled-password --home /home/container container && \
     chown -R container /var/lib/tor && chmod 700 /var/lib/tor && \
     rm -rf /var/lib/apt/lists/* && \
     tor --version
 
-COPY --chown=container torrc /etc/tor/
+COPY --chown=container torrc /home/container/
 
 USER container
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-GH_BASE_URL="https://github.com/Socks5Balancer/Socks5BalancerAsio/releases/download/v1.3.6-fix/Socks5BalancerAsio-e0a05953f1989bc3d7cbf776f2e88832fd313259"
-
-DOWNLOAD_URL="$GH_BASE_URL-DynamicSSL-ProxyHandshakeAuth-Linux.zip"
-#DOWNLOAD_URL="$GH_BASE_URL-DynamicSSL-Normal-Linux.zip"
-
-HOME_DIR="/home/container"
-TMP_DIR="$HOME_DIR/tmp"
-SOCKS5_RELAY_BIN="$HOME_DIR/socks5-relay"
-
-mkdir -p $TMP_DIR && \
-curl -L -o $TMP_DIR/socks5balancer.zip $DOWNLOAD_URL && \
-    unzip $TMP_DIR/socks5balancer.zip -d $TMP_DIR/socks5-relay && \
-    mv $TMP_DIR/socks5-relay/Socks5BalancerAsio "$SOCKS5_RELAY_BIN" && \
-    chmod 755 "$SOCKS5_RELAY_BIN" && \
-    rm -rf $TMP_DIR/socks5balancer.zip $TMP_DIR/socks5-relay
-
 cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
+
 {
   "listenHost": "::",
   "listenPort": 9150,
@@ -29,10 +14,11 @@ cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
     }
   ]
 }
+
 SOCKS5_CONF
 
-/home/container/socks5-relay -c /home/container/socks-relay-config.json &
+/usr/bin/socks5-relay -c /home/container/socks-relay-config.json &
 
-exec /usr/bin/tor -f /etc/tor/torrc
+exec /usr/bin/tor -f /home/container/torrc
 
 
