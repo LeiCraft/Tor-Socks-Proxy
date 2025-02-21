4621eaa1433292dd747ad4c356c6cf72128c2e26
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-02-21 13:21:48 +0000

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
+#AUTH=true
 
+TOR_SOCKS_CONF=""
+
+if [ "$AUTH" = true ]; then
+  TOR_SOCKS_CONF+="SocksPort 127.0.0.1:9151\n"
+  TOR_SOCKS_CONF+="SocksPort [::1]:9151\n"
+else
+  TOR_SOCKS_CONF+="SocksPort 0.0.0.0:9150\n"
+  TOR_SOCKS_CONF+="SocksPort [::]:9150\n"
+fi
+
+cat << TORRC > "/home/container/torrc"
+
+HardwareAccel 1
+Log notice stdout
+DataDirectory /var/lib/tor
+
+DNSPort 0.0.0.0:8853
+DNSPort [::]:8853
+
+$(echo -e $TOR_SOCKS_CONF)
+
+TORRC
+
+cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
 {
   "listenHost": "::",
   "listenPort": 9150,
@@ -12,12 +36,19 @@ cat << SOCKS5_CONF > "/home/container/socks-relay-config.json"
       "host": "127.0.0.1",
       "port": 9151
     }
+  ],
+  "AuthClientInfo": [
+    {
+      "user": "admin",
+      "pwd": "123456"
+    }
   ]
 }
-
 SOCKS5_CONF
 
-/usr/bin/socks5-relay -c /home/container/socks-relay-config.json &
+if [ "$AUTH" = true ]; then
+  /usr/bin/socks5-relay -c /home/container/socks-relay-config.json &
+fi
 
 exec /usr/bin/tor -f /home/container/torrc
 
